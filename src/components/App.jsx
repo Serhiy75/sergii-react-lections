0560ae710e6960b75bf885f2c16ed098235381d2
sstfnk3107 <s.stefanyk75@gmail.com>
2023-09-14 22:04:41 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import Link from './Link';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Link href="/" alt="home" text="Home" />
+      <br />
+      <Link href="/about" alt="about" text="About" />
+    </>
   );
 };
+
+//  style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
